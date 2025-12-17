b6061bafff1072b2b3fd5a60fba769fd032e37ff
Yashman-Singh <yashman.p.singh@gmail.com>
2025-12-17 23:52:19 +0530

    Initial Commit

---
+++ Drove/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Drove
+//
+//  Created by Yashman Singh on 12/17/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
